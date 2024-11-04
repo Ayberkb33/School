7e54299cff533b36957839a92ca5c5ce6d4159a0
AerKealy <alirizabeyhan@outlook.com>
2024-11-04 17:42:35 +0300

    Number Guess Game

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 
 int main (){
-srand(time(0));
-int num = (rand()% 6) +1;
+    std::cout << "************************Ready To Play***************************\n" ;
+    srand(time(0));
+    int number = (rand() % 200)+1;
+    int guess, tries;
+    
+    do{
+        std::cout << "Enter a Number = ";
+        std::cin >> guess;
+        tries++;
 
-switch(num){
-    case 1: std::cout << "You gain 5 gold";
-        break; 
-    case 2: std::cout << "You encounter an enemy";
-        break;
-    case 3: std::cout << "You lost 10 gold ";
-        break;
-    case 4: std::cout << "You fell in to a well";
-        break;
-    case 5: std::cout << "You encounter a friendly npc";
-        break;
-    case 6: std::cout << "JACKPOTT 100 GOLD !!";
-        break;
-}
-std::cout << "Working";
-return 0;
+        if(guess > number){
+            std::cout << "Too high\n";
+        }
+        
+        else if(guess < number){
+            std::cout << "Too low\n";
+        }
+
+        else{
+            std::cout << "you Won number was = " << number << "\n You took = " << tries << " tries\n";
+        }
+
+    }while(number != guess);
+    
+    std::cout << "**************************Game Over*****************************\n";
+
 }
     
