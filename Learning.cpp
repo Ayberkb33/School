d2a68658e6b6840f06d7bb314219824f1cc59043
AerKealy <alirizabeyhan@outlook.com>
2024-11-04 15:51:04 +0300

    new files

---
+++ Learning.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main (){
+srand(time(0));
+int num = (rand()% 6) +1;
+
+switch(num){
+    case 1: std::cout << "You gain 5 gold";
+        break; 
+    case 2: std::cout << "You encounter an enemy";
+        break;
+    case 3: std::cout << "You lost 10 gold ";
+        break;
+    case 4: std::cout << "You fell in to a well";
+        break;
+    case 5: std::cout << "You encounter a friendly npc";
+        break;
+    case 6: std::cout << "JACKPOTT 100 GOLD !!";
+        break;
+}
+return 0;
+}
+    
